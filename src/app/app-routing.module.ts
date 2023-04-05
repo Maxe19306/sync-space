8b5d662f561c87d1d7cfb2ff1a746681762a9acf
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-05 17:57:52 +0200

    routing for general view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { LoginComponent } from './login/login.component';
+import { GeneralViewComponent } from './general-view/general-view.component';
 
 const routes: Routes = [
   {path: 'signin', component: SignInComponent},
-  {path: '', component: LoginComponent}
+  {path: '', component: LoginComponent},
+  {path: 'generalView', component: GeneralViewComponent}
 ];
 
 @NgModule({
