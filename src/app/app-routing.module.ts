017f8d67a4e8599af55e56e046d006360620fd1a
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-05 17:10:23 +0200

    design login and sign in

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path: 'signin', component: SignInComponent}
+  {path: 'signin', component: SignInComponent},
+  {path: '', component: LoginComponent}
 ];
 
 @NgModule({
