2feb9a60281b3bcac5bffe924eb7ba83afca9713
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-07 13:46:11 +0200

    first steps for profile view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { LoginComponent } from './login/login.component';
 import { GeneralViewComponent } from './general-view/general-view.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
+import { ProfileViewComponent } from './profile-view/profile-view.component';
 
 const routes: Routes = [
   {path: 'signin', component: SignInComponent},
   {path: '', component: LoginComponent},
   {path: 'generalView', component: GeneralViewComponent},
-  {path: 'resetPassword', component: ResetPasswordComponent}
+  {path: 'resetPassword', component: ResetPasswordComponent},
+  {path: 'profil', component: ProfileViewComponent}
 ];
 
 @NgModule({
