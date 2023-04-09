742df0a0b4f31f242ac1d71b0f340bb05282675b
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-09 19:23:26 +0200

    add model currrentUser

---
+++ src/app/models/currentUser.class.ts
@@ -0,0 +1,6 @@
+export class currentUser{
+    id :string;
+    Name :string;
+    mail :string;
+    uid :string;
+}
--- src/app/profile-view/profile-view.component.ts
+++ src/app/profile-view/profile-view.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { currentUser } from '../models/currentUser.class';
+
 
 @Component({
   selector: 'app-profile-view',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+
+    CurrentUserID = 'M8SGwifEJntU27nZA4kR';
+    CurrentUser: currentUser[] = [];
+
 
   ngOnInit(): void {
+    this.loadCurrentUser()
   }
+
+  loadCurrentUser() {
+    this.firestore
+      .collection('users')
+      .doc(this.CurrentUserID)
+      .valueChanges({ idField: 'id' }) // idField hinzufügen, um TypeScript-Fehler zu vermeiden
+      .subscribe((user: currentUser) => {
+        this.CurrentUser = [user]; // als einzelnes Element in ein Array einfügen
+      });
+  }
+
 
 }
