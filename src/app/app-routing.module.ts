e48c5114fe5e2263f94769af16f1e735a15b64b9
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-09 13:49:32 +0200

    design channelview

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { GeneralViewComponent } from './general-view/general-view.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { ProfileViewComponent } from './profile-view/profile-view.component';
 import { ConfirnNewPasswordComponent } from './confirn-new-password/confirn-new-password.component';
+import { ChannelViewComponent } from './channel-view/channel-view.component';
 
 const routes: Routes = [
   {path: 'signin', component: SignInComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'generalView', component: GeneralViewComponent},
   {path: 'resetPassword', component: ResetPasswordComponent},
   {path: 'profil', component: ProfileViewComponent},
-  {path: 'confirmNewPassword', component: ConfirnNewPasswordComponent}
+  {path: 'confirmNewPassword', component: ConfirnNewPasswordComponent},
+  {path: 'channel', component: ChannelViewComponent}
 ];
 
 @NgModule({
