038a9c978d836ffb6def4db69312a2a437f1737c
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-17 16:42:31 +0200

    Channels are displayed in Channels Menu

--- src/app/channels-menu/channels-menu.component.ts
+++ src/app/channels-menu/channels-menu.component.ts
@@ -1,23 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { CreateChannelComponent } from '../create-channel/create-channel.component';
 import { MatDialog } from '@angular/material/dialog';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { DataService } from '../data.service';
+
 
 @Component({
   selector: 'app-channels-menu',
   templateUrl: './channels-menu.component.html',
-  styleUrls: ['./channels-menu.component.scss']
+  styleUrls: ['./channels-menu.component.scss'],
+  providers: [DataService],
 })
 export class ChannelsMenuComponent implements OnInit {
-
+  allChannels = []
   constructor(
+    private dataService : DataService,
+    public firestore: AngularFirestore,
     public Dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
+    this.firestore
+    .collection('channels')
+    .valueChanges({idField: 'channelIdName'})
+    .subscribe((channels) =>
+    {
+      this.allChannels = channels;
+      console.log(this.allChannels)
+    })
+    
   }
 
   openCreateNewChannelDialog(){
     this.Dialog.open(CreateChannelComponent)
+  }
+
+  test(channelID){
+    this.dataService.id = channelID
+    console.log(this.dataService.id)
   }
 
 }
--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -1,23 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ChannelViewComponent } from '../channel-view/channel-view.component';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-main-chat',
   templateUrl: './main-chat.component.html',
-  styleUrls: ['./main-chat.component.scss']
+  styleUrls: ['./main-chat.component.scss'],
+  providers: [DataService],
 })
 export class MainChatComponent implements OnInit {
 
   constructor(
+    private dataService : DataService,
     public Dialog: MatDialog,
   ) { }
 
   ngOnInit(): void {
+      console.log(this.dataService.id)
   }
 
 
   openDialogChannelView(){
     this.Dialog.open(ChannelViewComponent)
+    console.log(this.dataService.id)
 }
 }
