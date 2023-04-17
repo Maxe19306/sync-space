d78e62185ebaae17bec5f6e44eef526cef04bc4f
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-17 19:26:27 +0200

    detail view channel

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root'
+})
 export class DataService {
-  id: number;
+  id: string = 'xFuOtcoM2nZqdd49HDTB';
 }
--- src/app/dm-menu/dm-menu.component.ts
+++ src/app/dm-menu/dm-menu.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dm-menu.component.scss']
 })
 export class DmMenuComponent implements OnInit {
-
+  viewChannels = false;
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  viewNoChannels(){
+    this.viewChannels = false;
+  }
+
+  viewAllChannels(){
+    if(!this.viewChannels){
+      this.viewChannels = true;
+    }
+    else(this.viewChannels = false)
   }
 
 }
