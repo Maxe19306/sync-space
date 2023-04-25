943d6f8c16204f271c5f4f0ca991a433f935ad74
Christoph <c.wagner.germany@gmail.com>
2023-04-25 20:27:07 +0200

    chatForm add remove class

--- src/app/main-chat-footer/main-chat-footer.component.ts
+++ src/app/main-chat-footer/main-chat-footer.component.ts
@@ -10,32 +10,43 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 export class MainChatFooterComponent implements OnInit {
   currentUser
   message: Message = new Message({})
-  constructor(public dataService : DataService,
+  constructor(public dataService: DataService,
     private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
     this.loadCurrentUser()
-
+    this.styleChatForm();
   }
 
-  test(){
-    this.message.creator = this.currentUser; 
+  test() {
+    this.message.creator = this.currentUser;
     this.message.timestamp = new Date().getTime();
-     this.firestore
-     .collection('channels')
-     .doc(this.currentUser.lastChannel)
-     .collection('messages')
-     .add(this.message.toJSON())
+    this.firestore
+      .collection('channels')
+      .doc(this.currentUser.lastChannel)
+      .collection('messages')
+      .add(this.message.toJSON())
   }
 
-  loadCurrentUser(){
+  loadCurrentUser() {
     this.firestore
-     .collection('users')
-     .doc(this.dataService.id)
-     .valueChanges({idField: 'id'})
-     .subscribe((user) =>{
-      this.currentUser = user;
-     })
+      .collection('users')
+      .doc(this.dataService.id)
+      .valueChanges({ idField: 'id' })
+      .subscribe((user) => {
+        this.currentUser = user;
+      })
+  }
+
+  styleChatForm() {
+    const chatForm = document.getElementById("chatForm");
+    const chatTextarea = document.getElementById("chatTextarea");
+
+    chatTextarea.addEventListener("input", (event) => {
+      const chatTextarea = <HTMLInputElement>document.getElementById("chatTextarea");
+      if (chatTextarea.value.length > 0) chatForm.classList.add("form__active");
+      else chatForm.classList.remove("form__active");
+    });
   }
 
 }
