7d4fc7f92999ee4adda99516b40f1233c1535983
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-04-25 09:52:41 +0200

    view message in main chat body

--- src/app/main-chat-body/main-chat-body.component.ts
+++ src/app/main-chat-body/main-chat-body.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-main-chat-body',
@@ -6,10 +8,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-chat-body.component.scss']
 })
 export class MainChatBodyComponent implements OnInit {
-
-  constructor() { }
+  currentUser;
+  currentChannelMessage
+  constructor(
+    public dataService : DataService,
+    private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
+    this.loadCurrentUser()
   }
 
+
+  loadCurrentUser(){
+    this.firestore
+     .collection('users')
+     .doc(this.dataService.id)
+     .valueChanges({idField: 'id'})
+     .subscribe((user) =>{
+      this.currentUser = user;
+      this.loadChannel()
+     })
+  }
+
+  loadChannel(){
+    this.firestore
+     .collection('channels')
+     .doc(this.currentUser.lastChannel)
+     .collection('messages')
+     .valueChanges({idField: 'id'})
+     .subscribe((channel) =>{
+      this.currentChannelMessage = channel;
+      this.sortsMessages()
+      console.log(this.currentChannelMessage)
+
+     })
+  }
+
+  sortsMessages() {
+    this.currentChannelMessage.sort((a, b) => {
+      return Number(a.timestampe) - Number(b.timestampe);
+    });
+  }
 }
