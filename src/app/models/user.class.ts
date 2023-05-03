78f20de89bbf04c51fe5f09a06876cf874b9de9a
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-05-03 17:09:28 +0200

    fix user class

--- src/app/models/user.class.ts
+++ src/app/models/user.class.ts
@@ -3,12 +3,18 @@ export class User {
     mail: string;
     uid : string;
     lastChannel: string;
+    ChannelFromThread: string;
+    ThreadID : string;
+    openThread: boolean;
 
     constructor(obj: any){
         this.Name = obj ? obj.firstName : '';
         this.mail = obj ? obj.mail : '';
         this.uid = obj ? obj.uid : '';
         this.lastChannel = obj ? obj.lastChannel : 'PzdLMrdllSDRB9JYARxv';
+        this.ChannelFromThread = obj ? obj.ChannelFromThread : ''
+        this.ThreadID = obj ? obj.ThreadID : ''
+        this.openThread = false;
     }
 
     public toJSON() {
@@ -17,6 +23,8 @@ export class User {
             mail: this.mail,
             uid: this.uid,
             lastChannel: 'PzdLMrdllSDRB9JYARxv',
+            ChannelFromThread: '',
+            openThread: false
         }
     }
 
