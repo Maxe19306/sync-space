a205b25435fe3cdab9c1b86dd1ad3f582eec87ca
Christoph <c.wagner.germany@gmail.com>
2023-05-13 15:23:05 +0200

    scroll down onload

--- src/app/main-chat-body/main-chat-body.component.ts
+++ src/app/main-chat-body/main-chat-body.component.ts
@@ -10,13 +10,13 @@ import { MatDialog } from '@angular/material/dialog';
   styleUrls: ['./main-chat-body.component.scss']
 })
 export class MainChatBodyComponent implements OnInit {
- 
-Date = '';
+
+  Date = '';
   currentUser;
   currentChannelMessage
   constructor(
-     public Dialog: MatDialog,
-    public dataService : DataService,
+    public Dialog: MatDialog,
+    public dataService: DataService,
     private firestore: AngularFirestore) { }
 
   ngOnInit(): void {
@@ -24,29 +24,29 @@ Date = '';
   }
 
 
-  loadCurrentUser(){
+  loadCurrentUser() {
     this.firestore
-     .collection('users')
-     .doc(this.dataService.id)
-     .valueChanges({idField: 'id'})
-     .subscribe((user) =>{
-      this.currentUser = user;
-      this.loadMessageFromChannel()
-     })
+      .collection('users')
+      .doc(this.dataService.id)
+      .valueChanges({ idField: 'id' })
+      .subscribe((user) => {
+        this.currentUser = user;
+        this.loadMessageFromChannel()
+      })
   }
 
-  loadMessageFromChannel(){
+  loadMessageFromChannel() {
     this.firestore
-     .collection('channels')
-     .doc(this.currentUser.lastChannel)
-     .collection('messages')
-     .valueChanges({idField: 'messageID'})
-     .subscribe((channel) =>{
-      this.currentChannelMessage = channel;
-      console.log(this.currentChannelMessage)
-      this.sortsMessages()
+      .collection('channels')
+      .doc(this.currentUser.lastChannel)
+      .collection('messages')
+      .valueChanges({ idField: 'messageID' })
+      .subscribe((channel) => {
+        this.currentChannelMessage = channel;
+        console.log(this.currentChannelMessage)
+        this.sortsMessages()
 
-     })
+      })
   }
 
   sortsMessages() {
@@ -55,48 +55,48 @@ Date = '';
     });
   }
 
-  openDialogProfil(userID){
+  openDialogProfil(userID) {
     this.Dialog.open(ProfileViewComponent, {
-      data: {userID}
+      data: { userID }
     })
-}
+  }
 
-formatDate(timestamp) {
-  const date = new Date(timestamp);
-  const day = date.getDate().toString().padStart(2, '0');
-  const month = (date.getMonth() + 1).toString().padStart(2, '0');
-  const year = date.getFullYear().toString();
-  const hours = date.getHours().toString().padStart(2, '0');
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  return `${day}.${month}.${year} ${hours}:${minutes}`;
-}
+  formatDate(timestamp) {
+    const date = new Date(timestamp);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `${day}.${month}.${year} ${hours}:${minutes}`;
+  }
 
 
-openThread(Messageid, Channelid){
-  this.firestore
-  .collection('users')
-  .doc(this.dataService.id)
-  .update({
-    openThread : true,
-    ChannelFromThread: Channelid,
-    ThreadID : Messageid,
-  })
-}
+  openThread(Messageid, Channelid) {
+    this.firestore
+      .collection('users')
+      .doc(this.dataService.id)
+      .update({
+        openThread: true,
+        ChannelFromThread: Channelid,
+        ThreadID: Messageid,
+      })
+  }
 
-lastDateDisplayed(timestamp){
-// Date-Objekt aus dem Timestamp erstellen
-const date = new Date(timestamp);
-// Datum im Format "TT.MM.JJJJ" speichern
-const dateString = date.toLocaleDateString('de-DE');
-// Wenn das Datum mit dem zuletzt angezeigten Datum übereinstimmt, den Timestamp nicht anzeigen
-if (dateString === this.Date) {
-  return ''
-} else {
-  // Andernfalls das Datum im Timestamp anzeigen und die Variable aktualisieren
-  const newDate = dateString;
-  this.Date = newDate;
-  return newDate;
-}
-}
-  
+  lastDateDisplayed(timestamp) {
+    // Date-Objekt aus dem Timestamp erstellen
+    const date = new Date(timestamp);
+    // Datum im Format "TT.MM.JJJJ" speichern
+    const dateString = date.toLocaleDateString('de-DE');
+    // Wenn das Datum mit dem zuletzt angezeigten Datum übereinstimmt, den Timestamp nicht anzeigen
+    if (dateString === this.Date) {
+      return ''
+    } else {
+      // Andernfalls das Datum im Timestamp anzeigen und die Variable aktualisieren
+      const newDate = dateString;
+      this.Date = newDate;
+      return newDate;
+    }
+  }
+
 }
--- src/app/main-chat/main-chat.component.ts
+++ src/app/main-chat/main-chat.component.ts
@@ -10,53 +10,60 @@ import { MembersViewComponent } from '../members-view/members-view.component';
   templateUrl: './main-chat.component.html',
   styleUrls: ['./main-chat.component.scss'],
 })
+
 export class MainChatComponent implements OnInit {
   CurrentChannel;
   currentUser
   constructor(
-    public dataService : DataService,
+    public dataService: DataService,
     public Dialog: MatDialog,
     private firestore: AngularFirestore
   ) { }
 
   ngOnInit(): void {
-   this.loadCurrentUser()
+    this.loadCurrentUser()
+    this.scrollDownAfterPageLoad();
   }
 
-openDialogChannelView(ChannelId){
-  this.Dialog.open(ChannelViewComponent, {
-    data: {ChannelId}
-  })
-  
-}
-
-loadCurrentUser(){
-  this.firestore
-   .collection('users')
-   .doc(this.dataService.id)
-   .valueChanges({idField: 'id'})
-   .subscribe((user) =>{
-    this.currentUser = user;
-    this.loadLastChannel()
-   })
-}
-
-loadLastChannel(){
-  this.firestore
-  .collection('channels')
-  .doc(this.currentUser.lastChannel)
-  .valueChanges({idField: 'id'})
-  .subscribe((channel)=> {
-    this.CurrentChannel = channel
-  })
-}
-
-openDialogMembersView(members){
-  this.Dialog.open(MembersViewComponent,
-    {
-      data: {members}
+  openDialogChannelView(ChannelId) {
+    this.Dialog.open(ChannelViewComponent, {
+      data: { ChannelId }
     })
-}
 
+  }
 
+  loadCurrentUser() {
+    this.firestore
+      .collection('users')
+      .doc(this.dataService.id)
+      .valueChanges({ idField: 'id' })
+      .subscribe((user) => {
+        this.currentUser = user;
+        this.loadLastChannel()
+      })
+  }
+
+  loadLastChannel() {
+    this.firestore
+      .collection('channels')
+      .doc(this.currentUser.lastChannel)
+      .valueChanges({ idField: 'id' })
+      .subscribe((channel) => {
+        this.CurrentChannel = channel
+      })
+  }
+
+  openDialogMembersView(members) {
+    this.Dialog.open(MembersViewComponent,
+      {
+        data: { members }
+      })
+  }
+
+  scrollDownAfterPageLoad() {
+    document.addEventListener('DOMContentLoaded', function () {
+      let mainChatBody = document.getElementById("mainChatBody");
+      mainChatBody.scrollTop = mainChatBody.scrollHeight;
+    }, false);
+  }
 }
