d48eb3fc301b1148ce788d94c87f052e524b520a
Christoph <c.wagner.germany@gmail.com>
2023-05-30 19:29:13 +0200

    logo hover3

--- src/app/home-workspaces/home-workspaces.component.ts
+++ src/app/home-workspaces/home-workspaces.component.ts
@@ -11,9 +11,14 @@ export class HomeWorkspacesComponent implements OnInit {
   @ViewChild('logoDefault') logoDefault: ElementRef;
   @ViewChild('logoHover') logoHover: ElementRef;
 
+  @ViewChild('logoClosedContainer') logoClosedContainer: ElementRef;
+  @ViewChild('logoClosedDefault') logoClosedDefault: ElementRef;
+  @ViewChild('logoClosedHover') logoClosedHover: ElementRef;
+
   constructor(private sharedService: SharedService) { }
 
   toggleMenuSidebarLeft() {
+    this.toggleLogoDefaultClosed();
     this.sharedService.toggleMenuSidebarLeft();
   }
 
@@ -31,4 +36,27 @@ export class HomeWorkspacesComponent implements OnInit {
     this.logoHover.nativeElement.style.display = "none"
     this.logoDefault.nativeElement.style.display = "block";
   }
+
+  highlightClosedLogo() {
+    this.logoClosedDefault.nativeElement.style.display = "none"
+    this.logoClosedHover.nativeElement.style.display = "block";
+  }
+
+  unHighlightClosedLogo() {
+    this.logoClosedHover.nativeElement.style.display = "none"
+    this.logoClosedDefault.nativeElement.style.display = "block";
+  }
+
+  toggleLogoDefaultClosed() {
+    if (this.logoClosedContainer.nativeElement.style.display === "none") {
+      this.logoContainer.nativeElement.style.display = "none";
+      this.logoClosedContainer.nativeElement.style.display = "block";
+      this.logoClosedHover.nativeElement.style.display = "block";
+    }
+    else {
+      this.logoContainer.nativeElement.style.display = "block";
+      this.logoClosedContainer.nativeElement.style.display = "none"
+    }
+  }
+
 }
