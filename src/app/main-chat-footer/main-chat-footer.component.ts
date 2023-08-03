8f33aaa210c0261528429c07220803983aa35195
Christoph <c.wagner.germany@gmail.com>
2023-08-03 22:20:15 +0200

    emoji picker9

--- src/app/main-chat-footer/main-chat-footer.component.ts
+++ src/app/main-chat-footer/main-chat-footer.component.ts
@@ -102,7 +102,7 @@ export class MainChatFooterComponent implements OnInit {
   }
 
   handleSelection(event) {
-    // console.log(event.char);
+    console.log(event.char);
     const emojiPicker = document.getElementsByTagName('emoji-picker')[0] as HTMLElement;
     if (!this.message.text) {
       this.message.text = '';
