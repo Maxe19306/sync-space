bdbe6589d7371438ac563fc1663ca39a22186210
Christoph <c.wagner.germany@gmail.com>
2023-08-04 18:22:03 +0200

    emoji picker11

--- src/app/main-chat-footer/main-chat-footer.component.ts
+++ src/app/main-chat-footer/main-chat-footer.component.ts
@@ -101,9 +101,19 @@ export class MainChatFooterComponent implements OnInit {
     this.chatForm.classList.remove("form__active");
   }
 
+  styleEmojiPicker() {
+    const emojiPicker = document.getElementsByTagName('emoji-picker')[0] as HTMLElement;
+    setTimeout(() => {
+      emojiPicker.style.top = "0";
+    }, 500);
+  }
+
   handleSelection(event) {
     console.log(event.char);
     const emojiPicker = document.getElementsByTagName('emoji-picker')[0] as HTMLElement;
+    setTimeout(() => {
+      emojiPicker.style.top = "0";
+    }, 500);
     if (!this.message.text) {
       this.message.text = '';
     }
