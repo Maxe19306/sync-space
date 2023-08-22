698652927945f5bbe74c94d9d16be811e147d0b1
Christoph <c.wagner.germany@gmail.com>
2023-08-22 21:06:21 +0200

    format code4

--- src/app/secondary-chat-footer/secondary-chat-footer.component.spec.ts
+++ src/app/secondary-chat-footer/secondary-chat-footer.component.spec.ts
@@ -8,9 +8,9 @@ describe('SecondaryChatFooterComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SecondaryChatFooterComponent ]
+      declarations: [SecondaryChatFooterComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SecondaryChatFooterComponent);
     component = fixture.componentInstance;
