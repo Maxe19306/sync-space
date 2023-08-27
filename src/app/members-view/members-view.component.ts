f601121c92a64870cfedf2419e2fae7410e60efb
Christoph <c.wagner.germany@gmail.com>
2023-08-27 18:53:13 +0200

    add members8

--- src/app/members-view/members-view.component.ts
+++ src/app/members-view/members-view.component.ts
@@ -10,12 +10,12 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 })
 export class MembersViewComponent implements OnInit {
   test = true;
-  filteredUsers: any[]  = [];
+  filteredUsers: any[] = [];
   updateMembers = [];
   inputParticipants: string;
   addMembers = false;
   allUsers;
-  selectedUser: any[]  = [];
+  selectedUser: any[] = [];
   filterableUsers: any[] = [];
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<MembersViewComponent>,
@@ -29,15 +29,15 @@ export class MembersViewComponent implements OnInit {
 
   closeDialog() {
     this.dialogRef.close(MembersViewComponent);
-    }
+  }
 
-    test2(){
-      this.test = this.data.test
-      if(!this.test){
-        this.addMembers = true;
-      }
+  test2() {
+    this.test = this.data.test
+    if (!this.test) {
+      this.addMembers = true;
     }
-    
+  }
+
   loadAllUsers() {
     this.firestore
       .collection('users')
@@ -48,7 +48,7 @@ export class MembersViewComponent implements OnInit {
         this.getCurrentInfoOfTheUsers()
       })
   }
-  
+
   openDialogProfil(userId) {
     this.Dialog.open(ProfileViewComponent, {
       data: { userId },
@@ -57,80 +57,87 @@ export class MembersViewComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  activAddMembers(){
+  activAddMembers() {
     this.addMembers = true;
   }
-  
-  createTheFilterableUsers(){
+
+  createTheFilterableUsers() {
     const nonMembers = this.allUsers.filter(user => !this.data.members.some(member => member.id === user.id));
-       this.filterableUsers.push(nonMembers)
+    this.filterableUsers.push(nonMembers)
   }
-  
+
   filterUser() {
     if (this.filterableUsers && this.filterableUsers.length > 0 && this.inputParticipants.length > 0) {
       if (this.filterableUsers[0]) {
         this.filteredUsers = this.filterableUsers[0].filter(user =>
           user.name.toLowerCase().includes(this.inputParticipants.toLowerCase())
-        )} 
+        )
+      }
     } else {
       this.filteredUsers = [];
     }
   }
-    
- 
+
+
   getCurrentInfoOfTheUsers() {
-      this.data.members.forEach(member => {
+    this.data.members.forEach(member => {
       this.firestore
         .collection('users')
         .doc(member.id)
-        .valueChanges({idField: 'id'})
+        .valueChanges({ idField: 'id' })
         .subscribe((memberInfo) => {
           // Überprüfe, ob die Informationen bereits in der Liste sind
           const index = this.updateMembers.findIndex(existingMember => existingMember.id === memberInfo.id);
-  
+
           if (index === -1) {
             this.updateMembers.push(memberInfo); // Speichere die aktualisierten Informationen
           }
         });
-    }); 
-  } 
-  
-  
-  deleteMember(user){
+    });
+  }
+
+
+  deleteMember(user) {
     this.filterableUsers.push(user)
     const userIndex = this.selectedUser.indexOf(user)
-    
-    if(userIndex !== -1){
-      this.data.members.splice(userIndex,1)
+
+    if (userIndex !== -1) {
+      this.data.members.splice(userIndex, 1)
     }
     this.filterUser()
-  } 
-    
+  }
+
   pushUserToMember(user) {
     this.selectedUser.push(user);
-    
+
     const userIndex1 = this.filterableUsers.indexOf(user)
     const userIndex2 = this.filteredUsers.indexOf(user)
-    
-    if(userIndex1 && userIndex2 !== -1)
-    this.filterableUsers.splice(userIndex1,1)
-    this.filteredUsers.splice(userIndex2,1)
+
+    if (userIndex1 && userIndex2 !== -1)
+      this.filterableUsers.splice(userIndex1, 1)
+    this.filteredUsers.splice(userIndex2, 1)
+
+    this.displayClickedUser(user);
   }
-  
-  
-  addNewMembersToChannel(){
-   this.selectedUser.forEach(user => {
+
+  displayClickedUser(clickedUser) {
+
+  }
+
+
+  addNewMembersToChannel() {
+    this.selectedUser.forEach(user => {
       this.data.members.push(user)
-   });
-   
-   
-   this.firestore
-   .collection('channels')
-   .doc(this.data.channelId)
-   .update({
-    members: this.data.members
-   })
-   
+    });
+
+
+    this.firestore
+      .collection('channels')
+      .doc(this.data.channelId)
+      .update({
+        members: this.data.members
+      })
+
   }
-  
+
 }
