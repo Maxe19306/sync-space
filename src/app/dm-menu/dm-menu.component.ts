790e74a4dba0eb6a23856fca4ac3b1b7d65ca8fa
Maximilian Bruhn <maximilianbruhn@gmx.de>
2023-09-03 11:10:52 +0200

    fix dm menu bug

--- src/app/dm-menu/dm-menu.component.ts
+++ src/app/dm-menu/dm-menu.component.ts
@@ -5,27 +5,24 @@ import { AngularFirestore } from '@angular/fire/compat/firestore';
 @Component({
   selector: 'app-dm-menu',
   templateUrl: './dm-menu.component.html',
-  styleUrls: ['./dm-menu.component.scss']
+  styleUrls: ['./dm-menu.component.scss'],
 })
-
 export class DmMenuComponent implements OnInit {
-
   mobileBreakpointGeneral: number;
   viewChannels = false;
-  personalDm
-  allDmsIdFromUser;
+  personalDm;
   allDmsFromUser = [];
-  currentUser
+  currentUser;
+  finalDm = [];
 
   constructor(
     public dataService: DataService,
     private firestore: AngularFirestore
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.getMobileBreakpointGeneral();
-    this.loadAllDms()
-    this.loadCurrentUser()
+    this.loadCurrentUser();
   }
 
   getMobileBreakpointGeneral() {
@@ -41,35 +38,20 @@ export class DmMenuComponent implements OnInit {
       .valueChanges({ idField: 'id' })
       .subscribe((user) => {
         this.currentUser = user;
-      })
+        this.loadAllDms();
+      });
   }
 
   loadAllDms() {
     this.firestore
       .collection('users')
-      .doc(this.dataService.id)
+      .doc(this.currentUser.id)
       .collection('dmsFromUser')
       .valueChanges({ idField: 'id' })
       .subscribe((dm) => {
-        this.allDmsIdFromUser = dm
-        this.loadAllChats()
-      }
-      )
+        this.loadAllChats(dm); // lädt alle chat die im user hinterlegt wird herunter und ruft funktion auf, die diese dms herunterlädt
+      });
   }
-
-  determinePersonalChat() {
-    this.allDmsFromUser.forEach(element => {
-      const member1 = element.members[0]
-      const member2 = element.members[1]
-
-      if (member1.id === member2.id) {
-        this.personalDm = element
-      }
-      
-      console.log(this.allDmsFromUser)
-
-    });
-  } 
 
   viewNoChannels() {
     this.viewChannels = false;
@@ -78,8 +60,7 @@ export class DmMenuComponent implements OnInit {
   viewAllDMS() {
     if (!this.viewChannels) {
       this.viewChannels = true;
-    }
-    else {
+    } else {
       this.dmSlideUpClass();
       setTimeout(() => {
         this.viewChannels = false;
@@ -88,40 +69,61 @@ export class DmMenuComponent implements OnInit {
   }
 
   dmSlideUpClass() {
-    const dmChannelsBody = document.getElementById("dmChannelsBody") as HTMLDivElement;
-    dmChannelsBody.classList.remove("drop__drown__animation");
-    dmChannelsBody.classList.add("slide__up__animation");
+    const dmChannelsBody = document.getElementById(
+      'dmChannelsBody'
+    ) as HTMLDivElement;
+    dmChannelsBody.classList.remove('drop__drown__animation');
+    dmChannelsBody.classList.add('slide__up__animation');
   }
 
-  loadAllChats() {
-    this.allDmsIdFromUser.forEach(dm => {
+  loadAllChats(dm) {
+    dm.forEach((dms) => {
       this.firestore
         .collection('dms')
-        .doc(dm.DMID)
+        .doc(dms.DMID)
         .valueChanges({ idField: 'DmId' })
-        .subscribe(dmData => {
-            const existingDm = this.allDmsFromUser.find(d => d.DmId === dmData.DmId); //Hier wird versucht, in allDmsFromUser nach einer vorhandenen dm mit der gleichen id zu suchen. Dies geschieht, um Duplikate zu vermeiden.
-          if (!existingDm) {
-            this.allDmsFromUser.push(dmData);
-            this.determinePersonalChat() 
-          }
-        } );
+        .subscribe((dmData) => {
+          this.determineDM(dmData);
+        });
+    });
+  }
+
+  determineDM(dmData) {
+    if (dmData.members[0].id === dmData.members[1].id) {
+      this.personalDm = dmData.DmId;
+    } else this.loadFinalDms(dmData);
+  }
+
+  loadFinalDms(dm) {
+    dm.members.forEach((user) => {
+      if (user.id !== this.currentUser.id) {
+        const dmId = dm.DmId;
+        this.firestore
+          .collection('users')
+          .doc(user.id)
+          .valueChanges({ idField: 'DmId' })
+          .subscribe((finalUser) => {
+            const isDmIdInFinalDm = this.finalDm.some(
+              (item) => item.dmId === dmId
+            );
+            if (!isDmIdInFinalDm) { // wird nur hinzugefügt wenn die Dmid noch nicht enthalten ist. Sonst entstehen duplikate und der chat wird öfter angezeigt.
+              this.finalDm.push({ finalUser, dmId });
+            }
+          });
+      }
     });
   }
 
   openChat(id) {
     this.foregroundMainDirectMessageAttributs();
-    this.firestore
-      .collection('users')
-      .doc(this.dataService.id)
-      .update({
-        viewChat: true,
-        currentDM: id
-      })
-      if (window.innerWidth <= this.mobileBreakpointGeneral) {
-        this.foregroundMainDirectMessage();
-      }
-      this.hideSecondaryChat();
+    this.firestore.collection('users').doc(this.dataService.id).update({
+      viewChat: true,
+      currentDM: id,
+    });
+    if (window.innerWidth <= this.mobileBreakpointGeneral) {
+      this.foregroundMainDirectMessage();
+    }
+    this.hideSecondaryChat();
   }
 
   foregroundMainDirectMessage() {
@@ -131,33 +133,41 @@ export class DmMenuComponent implements OnInit {
   }
 
   hideMenuSidebarLeft() {
-    const sidebarLeft = document.getElementsByTagName("app-menu-sidebar-left")[0] as HTMLElement;
+    const sidebarLeft = document.getElementsByTagName(
+      'app-menu-sidebar-left'
+    )[0] as HTMLElement;
     if (sidebarLeft) {
       sidebarLeft.style.display = 'none';
     }
-    const menuSidebarLeftFAB = document.getElementById("menuSidebarLeftFAB");
-    menuSidebarLeftFAB.style.display = "none";
+    const menuSidebarLeftFAB = document.getElementById('menuSidebarLeftFAB');
+    menuSidebarLeftFAB.style.display = 'none';
   }
 
   changeLogoToBtn() {
-    const logoMobile = document.getElementById("logoMobile");
-    logoMobile.style.display = "none";
-    const channelsButtonMobile = document.getElementById("channelsButtonMobile");
-    channelsButtonMobile.style.display = "block";
+    const logoMobile = document.getElementById('logoMobile');
+    logoMobile.style.display = 'none';
+    const channelsButtonMobile = document.getElementById(
+      'channelsButtonMobile'
+    );
+    channelsButtonMobile.style.display = 'block';
   }
 
   hideSecondaryChat() {
-    const secondaryChat = document.getElementsByTagName("app-secondary-chat")[0] as HTMLElement;
+    const secondaryChat = document.getElementsByTagName(
+      'app-secondary-chat'
+    )[0] as HTMLElement;
     if (secondaryChat) {
-      secondaryChat.style.display = "none";
-      secondaryChat.style.width= "auto";
-      secondaryChat.style.marginLeft = 25 + "px";
-      secondaryChat.style.backgroundColor = "white";
+      secondaryChat.style.display = 'none';
+      secondaryChat.style.width = 'auto';
+      secondaryChat.style.marginLeft = 25 + 'px';
+      secondaryChat.style.backgroundColor = 'white';
     }
   }
 
   foregroundMainDirectMessageAttributs() {
-    const mainDirectMessages = document.getElementsByTagName("app-main-direct-message");
+    const mainDirectMessages = document.getElementsByTagName(
+      'app-main-direct-message'
+    );
     if (mainDirectMessages.length > 0) {
       const mainDirectMessage = mainDirectMessages[0] as HTMLElement;
       mainDirectMessage.style.display = 'block';
